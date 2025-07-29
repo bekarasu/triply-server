d8b1c622aca9abc52a6b64da7854c7654a12b4f4
burak.karasu <burak.karasu@sisal.com>
2025-07-29 11:00:39 +0300

    Recommendation DB Renaming

--- recommandation-service/database_migrations/init.sql
+++ recommandation-service/database_migrations/init.sql
@@ -1,8 +1,9 @@
 -- this script must be run in the PostgreSQL container first by root user
+-- this script must be run line by line
 
-CREATE DATABASE recommandation_service OWNER triply_user;
-GRANT ALL PRIVILEGES ON DATABASE recommandation_service TO triply_user;
-\c recommandation_service;
+CREATE DATABASE recommendation_service OWNER triply_user;
+GRANT ALL PRIVILEGES ON DATABASE recommendation_service TO triply_user;
+\c recommendation_service;
 GRANT USAGE ON SCHEMA public TO triply_user;
 GRANT CREATE ON SCHEMA public TO triply_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO triply_user;
